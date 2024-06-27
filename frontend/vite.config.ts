9873445d12832bd41f46958fed0598d122a504a2
david-cc <david.caudeli@web-education.net>
2024-06-27 17:41:47 +0200

    feat: #WB2-1753, migrate app to React (#22)

---
+++ frontend/vite.config.ts
@@ -0,0 +1,107 @@
+/// <reference types='vitest' />
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+
+export default ({ mode }: { mode: string }) => {
+  // Checking environement files
+  const envFile = loadEnv(mode, process.cwd());
+  const envs = { ...process.env, ...envFile };
+  const hasEnvFile = Object.keys(envFile).length;
+
+  // Proxy variables
+  const headers = hasEnvFile
+    ? {
+        'set-cookie': [
+          `oneSessionId=${envs.VITE_ONE_SESSION_ID}`,
+          `XSRF-TOKEN=${envs.VITE_XSRF_TOKEN}`,
+        ],
+        'Cache-Control': 'public, max-age=300',
+      }
+    : {};
+
+  const proxyObj = hasEnvFile
+    ? {
+        target: envs.VITE_RECETTE,
+        changeOrigin: true,
+        headers: {
+          cookie: `oneSessionId=${envs.VITE_ONE_SESSION_ID};authenticated=true; XSRF-TOKEN=${envs.VITE_XSRF_TOKEN}`,
+        },
+      }
+    : {
+        target: 'http://localhost:8090',
+        changeOrigin: false,
+      };
+
+  /* Replace "/" the name of your application (e.g : blog | mindmap | collaborativewall) */
+  return defineConfig({
+    base: mode === 'production' ? '/wiki' : '',
+    root: __dirname,
+    cacheDir: './node_modules/.vite/wiki',
+
+    server: {
+      proxy: {
+        '/applications-list': proxyObj,
+        '/conf/public': proxyObj,
+        '^/(?=help-1d|help-2d)': proxyObj,
+        '^/(?=assets)': proxyObj,
+        '^/(?=theme|locale|i18n|skin)': proxyObj,
+        '^/(?=auth|appregistry|cas|userbook|directory|communication|conversation|portal|session|timeline|workspace|infra)':
+          proxyObj,
+        '/explorer': proxyObj,
+        '/wiki': proxyObj,
+      },
+      port: 4200,
+      headers,
+      host: 'localhost',
+    },
+
+    preview: {
+      port: 4300,
+      headers,
+      host: 'localhost',
+    },
+
+    plugins: [react(), nxViteTsPaths()],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    build: {
+      outDir: './dist',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+      assetsDir: 'public',
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        external: ['edifice-ts-client'],
+        output: {
+          paths: {
+            'edifice-ts-client': '/assets/js/edifice-ts-client/index.js',
+          },
+        },
+      },
+    },
+
+    test: {
+      watch: false,
+      globals: true,
+      cache: {
+        dir: './node_modules/.vitest/wiki',
+      },
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: './coverage/wiki',
+        provider: 'v8',
+      },
+    },
+  });
+};
