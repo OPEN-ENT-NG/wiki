cf9cfea527e1fac92f834a28d566644d6a445ee1
Clement Creusat <clement.creusat@gmail.com>
2024-07-12 17:26:17 +0200

    chore: clean tests files

--- frontend/src/components/WikiEmptyScreen/index.spec.tsx
+++ frontend/src/components/WikiEmptyScreen/index.spec.tsx
@@ -9,16 +9,14 @@ describe('WikiEmptyScreen', () => {
     expect(baseElement).toBeTruthy();
   });
 
-  describe('test title to be rendered', () => {
-    it('title is rendered', () => {
-      const { getByTestId } = render(<WikiEmptyScreen />);
-      expect(getByTestId('emptyscreen-title')).toBeInTheDocument();
-    });
+  it('should render a title', () => {
+    const { getByTestId } = render(<WikiEmptyScreen />);
+    expect(getByTestId('emptyscreen-title')).toBeInTheDocument();
+  });
 
-    it('title should be h2 level', () => {
-      const { getByRole } = render(<WikiEmptyScreen />);
-      expect(getByRole('heading', { level: 2 }));
-    });
+  it('title should be h2 level', () => {
+    const { getByRole } = render(<WikiEmptyScreen />);
+    expect(getByRole('heading', { level: 2 }));
   });
 
   it('should render a text', () => {
