0f612468680042bdc33a5c0e976c3869102e4e08
Clement Creusat <clement.creusat@gmail.com>
2024-07-12 16:32:50 +0200

    wip: test with react router

--- frontend/src/routes/__tests__/wiki/index.spec.tsx
+++ frontend/src/routes/__tests__/wiki/index.spec.tsx
@@ -1,28 +1,55 @@
-import { render } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-import { Index } from '~/routes/wiki';
+import { render, screen, waitFor } from '~/mocks/setup.vitest';
+import { Index } from '~/routes/wiki/index';
 
-describe('Create new page button', () => {
-  beforeEach(async () => {
-    const FAKE_EVENT = { pages: [] };
-    const routes = [
+it('render Index component', async () => {
+  const FAKE_EVENT = { name: 'test event' };
+  const routes = [
+    {
+      path: '/events/:id',
+      element: <Index />,
+      loader: () => FAKE_EVENT,
+    },
+  ];
+
+  const router = createMemoryRouter(routes, {
+    initialEntries: ['/', '/events/123'],
+    initialIndex: 1,
+  });
+
+  render(<RouterProvider router={router} />);
+
+  await waitFor(() => screen.getByTestId('text'));
+  expect(screen.getByTestId('text')).toHaveTextContent('some text');
+  /* const FAKE_EVENT = { pages: [] };
+    const route = '/wiki/123456';
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: '/wiki/:id',
+          loader: () => FAKE_EVENT,
+          element: <Index />,
+        },
+      ],
       {
-        path: '/id/:id',
-        loader: () => FAKE_EVENT,
-        element: <Index />,
-      },
-    ];
+        initialEntries: [route],
+        initialIndex: 0,
+      }
+    );
 
-    const router = createMemoryRouter(routes, {
-      initialEntries: ['/', '/id/123'],
-      initialIndex: 1,
-    });
+    render(<RouterProvider router={router} />); */
 
-    // await waitFor(() => render(<RouterProvider router={router} />));
-    render(<RouterProvider router={router} />);
-  });
+  /* render(
+      <MemoryRouter initialEntries={[route]}>
+        <Index />
+      </MemoryRouter>
+    ); */
 
-  it('render Index component', () => {
-    console.log('coucou');
-  });
+  // verify location display is rendered
+  /* expect(screen.getByRole('button')).toHaveTextContent(
+      'wiki.create.new.page'
+    ); */
+
+  screen.debug();
 });
