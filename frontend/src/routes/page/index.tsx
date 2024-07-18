fb1b314a1b07e1b01346a6fcd96544f605377895
Clement Creusat <clement.creusat@gmail.com>
2024-07-18 18:27:19 +0200

    wip: #WB2-1603, add delete modal

--- frontend/src/routes/page/index.tsx
+++ frontend/src/routes/page/index.tsx
@@ -1,4 +1,4 @@
-import { LoadingScreen } from '@edifice-ui/react';
+import { Button, LoadingScreen, Modal } from '@edifice-ui/react';
 import { QueryClient } from '@tanstack/react-query';
 import { odeServices } from 'edifice-ts-client';
 import {
@@ -8,7 +8,13 @@ import {
   redirect,
   useParams,
 } from 'react-router-dom';
-import { pageQueryOptions, useGetPage, wikiService } from '~/services';
+import {
+  pageQueryOptions,
+  useGetPage,
+  wikiQueryOptions,
+  wikiService,
+} from '~/services';
+import { getWikiActions, useOpenDeleteModal } from '~/store';
 
 export const loader =
   (queryClient: QueryClient) =>
@@ -30,13 +36,19 @@ export const loader =
     return data;
   };
 
-export async function action({ params }: ActionFunctionArgs) {
-  await wikiService.deletePage({
-    wikiId: params.wikiId!,
-    pageId: params.pageId!,
-  });
-  return redirect(`/id/${params.wikiId}`);
-}
+export const action = (queryClient: QueryClient) =>
+  async function action({ params }: ActionFunctionArgs) {
+    const result = await wikiService.deletePage({
+      wikiId: params.wikiId!,
+      pageId: params.pageId!,
+    });
+
+    console.log({ result });
+
+    await queryClient.invalidateQueries({ queryKey: wikiQueryOptions.base });
+
+    return redirect(`/id/${params.wikiId}`);
+  };
 
 export const Page = () => {
   const params = useParams();
@@ -46,16 +58,61 @@ export const Page = () => {
     pageId: params.pageId!,
   });
 
+  const openDeleteModal = useOpenDeleteModal();
+  const { setOpenDeleteModal } = getWikiActions();
+
   if (isPending) return <LoadingScreen />;
 
   if (error) return 'An error has occurred: ' + error.message;
 
+  const hasChildren = false;
+
   return (
     <>
       <div>{data.title}</div>
-      <Form method="delete">
-        <button type="submit">supprimer page</button>
-      </Form>
+      <button type="button" onClick={() => setOpenDeleteModal(true)}>
+        supprimer page
+      </button>
+      {openDeleteModal && (
+        <Modal
+          id="delete-page"
+          isOpen={openDeleteModal}
+          onModalClose={() => setOpenDeleteModal(false)}
+        >
+          <Modal.Header onModalClose={() => setOpenDeleteModal(false)}>
+            {!hasChildren
+              ? 'Suppression de la page'
+              : 'Suppression des pages et sous-pages'}
+          </Modal.Header>
+          <Modal.Subtitle>
+            {!hasChildren
+              ? 'Souhaitez-vous supprimer la page ?'
+              : 'Souhaitez-vous supprimer la page et ses sous-pages ?'}
+          </Modal.Subtitle>
+          <Modal.Body>&nbsp;</Modal.Body>
+          <Modal.Footer>
+            <Button
+              type="button"
+              color="tertiary"
+              variant="ghost"
+              onClick={() => setOpenDeleteModal(false)}
+            >
+              Annuler
+            </Button>
+            <Form
+              action="destroy"
+              method="post"
+              onSubmit={() => setOpenDeleteModal(false)}
+            >
+              <Button type="submit" color="danger" variant="filled">
+                {!hasChildren
+                  ? 'Supprimer la page'
+                  : 'Supprimer la page et les sous-pages'}
+              </Button>
+            </Form>
+          </Modal.Footer>
+        </Modal>
+      )}
     </>
   );
 };
