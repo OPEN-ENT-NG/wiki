6cc086f548e5818c8a889a971df576526501ba41
Clement Creusat <clement.creusat@gmail.com>
2024-07-24 18:33:42 +0200

    fix: add useDirectory hook

--- frontend/src/features/wiki/ContentTitle.tsx
+++ frontend/src/features/wiki/ContentTitle.tsx
@@ -1,12 +1,12 @@
-import { Avatar, useDate, useOdeClient } from '@edifice-ui/react';
+import { Avatar, useDate, useDirectory, useOdeClient } from '@edifice-ui/react';
 import { ID } from 'edifice-ts-client';
-import { Page } from '~/models';
 import { useTranslation } from 'react-i18next';
-import { getAvatarURL, getUserbookURL } from '~/utils/AuthorUtils';
+import { Page } from '~/models';
 
 export const ContentTitle = ({ page }: { page: Page }) => {
   const { formatDate } = useDate();
   const { appCode } = useOdeClient();
+  const { getAvatarURL, getUserbookURL } = useDirectory();
 
   const { t } = useTranslation(appCode);
 
@@ -19,11 +19,14 @@ export const ContentTitle = ({ page }: { page: Page }) => {
         <Avatar
           alt={t('wiki.consul.author.avatar')}
           size="sm"
-          src={getAvatarURL(page.author as ID)}
+          src={getAvatarURL(page.author as ID, 'user')}
           variant="circle"
         />
         <div className="text-gray-700 small d-flex flex-column flex-md-row column-gap-12 align-items-md-center ">
-          <a href={getUserbookURL(page.author as ID)} className="page-author">
+          <a
+            href={getUserbookURL(page.author as ID, 'user')}
+            className="page-author"
+          >
             {page.authorName}
           </a>
           <span className="separator d-none d-md-block"></span>
--- frontend/src/utils/AuthorUtils.ts
+++
@@ -1,9 +0,0 @@
-import { ID, odeServices } from 'edifice-ts-client';
-
-export function getAvatarURL(userId: ID): string {
-  return odeServices.directory().getAvatarUrl(userId, 'user');
-}
-
-export function getUserbookURL(userId: ID): string {
-  return odeServices.directory().getDirectoryUrl(userId, 'user');
-}
