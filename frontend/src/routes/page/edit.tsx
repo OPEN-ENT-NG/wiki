a3e8a08248efe659442c8b527cda1ae8ad6a6b5f
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2024-07-25 15:46:55 +0200

    fix: #WB2-1530, add queryclient for action edit

--- frontend/src/routes/page/edit.tsx
+++ frontend/src/routes/page/edit.tsx
@@ -1,27 +1,29 @@
 import { LoadingScreen } from '@edifice-ui/react';
+import { QueryClient } from '@tanstack/react-query';
 import { ActionFunctionArgs, redirect, useParams } from 'react-router-dom';
 import { FormPage } from '~/features/page/FormPage';
-import { queryClient } from '~/providers';
 import { useGetPage, wikiQueryOptions, wikiService } from '~/services';
 
-export async function action({ params, request }: ActionFunctionArgs) {
-  const formData = await request.formData();
-  const title = formData.get('title') as string;
-  const content = formData.get('content') as string;
+export const action =
+  (queryClient: QueryClient) =>
+  async ({ params, request }: ActionFunctionArgs) => {
+    const formData = await request.formData();
+    const title = formData.get('title') as string;
+    const content = formData.get('content') as string;
 
-  await wikiService.updatePage({
-    wikiId: params.wikiId!,
-    pageId: params.pageId!,
-    data: {
-      title,
-      content,
-    },
-  });
+    await wikiService.updatePage({
+      wikiId: params.wikiId!,
+      pageId: params.pageId!,
+      data: {
+        title,
+        content,
+      },
+    });
 
-  await queryClient.invalidateQueries({ queryKey: wikiQueryOptions.base });
+    await queryClient.invalidateQueries({ queryKey: wikiQueryOptions.base });
 
-  return redirect(`/id/${params.wikiId}/page/${params.pageId!}`);
-}
+    return redirect(`/id/${params.wikiId}/page/${params.pageId!}`);
+  };
 
 export const EditPage = () => {
   const params = useParams();
