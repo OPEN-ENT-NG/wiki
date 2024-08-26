b713ea6e27f2c0fb78823578813afec5c94c9854
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2024-08-26 16:50:03 +0200

    fix: #WB2-1515, fix right to show button new page

--- frontend/src/features/wiki/NewPage.tsx
+++ frontend/src/features/wiki/NewPage.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 
 export const NewPage = () => {
   const navigate = useNavigate();
-
   const { appCode } = useOdeClient();
   const { t } = useTranslation();
 
