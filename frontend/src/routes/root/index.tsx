9c954de1752196e25db1785fc8c5b4c683303179
Clément Creusat <clement.creusat@gmail.com>
2024-09-12 15:22:01 +0200

    feat: #WB2-1834, change redirection wiki and pages with new frontend routes (#61)

--- frontend/src/routes/root/index.tsx
+++ frontend/src/routes/root/index.tsx
@@ -3,6 +3,12 @@ import { Layout, LoadingScreen, useOdeClient } from '@edifice-ui/react';
 import { matchPath, Outlet, ScrollRestoration } from 'react-router-dom';
 import { basename } from '..';
 
+function redirectTo(redirectPath: string) {
+  return window.location.replace(
+    window.location.origin + basename.replace(/\/$/g, '') + redirectPath
+  );
+}
+
 /** Check old format URL and redirect if needed */
 export const loader = async () => {
   const hashLocation = window.location.hash.substring(1);
@@ -10,13 +16,18 @@ export const loader = async () => {
   // Check if the URL is an old format (angular root with hash) and redirect to the new format
   if (hashLocation) {
     const isPath = matchPath('/view/:id', hashLocation);
+    const isPage = matchPath('/view/:id/:idpage', hashLocation);
 
     if (isPath) {
       // Redirect to the new format
       const redirectPath = `/id/${isPath?.params.id}`;
-      window.location.replace(
-        window.location.origin + basename.replace(/\/$/g, '') + redirectPath
-      );
+      redirectTo(redirectPath);
+    }
+
+    if (isPage) {
+      // Redirect to the new format
+      const redirectPath = `/id/${isPage?.params.id}/page/${isPage?.params.idpage}`;
+      redirectTo(redirectPath);
     }
   }
 
