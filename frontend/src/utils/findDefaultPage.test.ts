8a5cb3c8b518e765561a6b4a4d8e42e7282ad8dc
david-cc <david.caudeli@web-education.net>
2024-10-01 14:59:21 +0200

    fix: #WB2-1925, redirect to a visible default page (#81)

---
+++ frontend/src/utils/findDefaultPage.test.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect } from 'vitest';
+import { findDefaultPage } from './findDefaultPage';
+import {
+  mockUserContrib,
+  mockUserManager,
+  mockUserRead,
+  mockWiki,
+  mockWikiWithHiddenIndexPage,
+  mockWikiWithOnlyHiddenPages,
+} from '~/mocks';
+
+describe('findDefaultPage', () => {
+  it('should return the index page if the user is a manager', () => {
+    const result = findDefaultPage(mockWiki, mockUserManager);
+    expect(result?._id).toBe(mockWiki.index);
+  });
+
+  it('should return the index page if the user is a reader and index page is visible', () => {
+    const result = findDefaultPage(mockWiki, mockUserRead);
+    expect(result?._id).toBe(mockWiki.index);
+  });
+
+  it('should return the first visible page if the index page is not visible and user is not manager', () => {
+    const firstVisiblePageId = '003';
+    const result = findDefaultPage(
+      mockWikiWithHiddenIndexPage,
+      mockUserContrib
+    );
+    expect(result?._id).toBe(firstVisiblePageId);
+  });
+
+  it('should return the index page if the user is a manager and the index page is not visible', () => {
+    const result = findDefaultPage(
+      mockWikiWithHiddenIndexPage,
+      mockUserManager
+    );
+    expect(result?._id).toBe(mockWikiWithHiddenIndexPage.index);
+  });
+
+  it('should return undefined if no visible page is found and user is not a manager', () => {
+    const result = findDefaultPage(mockWikiWithOnlyHiddenPages, mockUserRead);
+    expect(result).toBeUndefined();
+  });
+});
