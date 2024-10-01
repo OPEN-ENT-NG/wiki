8c657f9bd529a7130324821ff88ce6c82295dab1
Clement Creusat <clement.creusat@gmail.com>
2024-10-01 10:16:44 +0200

    feat: #WB2-1536, add actions to List component

--- frontend/src/routes/page/list.tsx
+++ frontend/src/routes/page/list.tsx
@@ -1,8 +1,17 @@
 import {
+  Copy,
+  Delete,
+  FolderMove,
+  Forgoing,
+  Print,
+  See,
+} from '@edifice-ui/icons';
+import {
   Badge,
   Heading,
   List,
   LoadingScreen,
+  ToolbarItem,
   useDate,
 } from '@edifice-ui/react';
 import { QueryClient } from '@tanstack/react-query';
@@ -13,12 +22,18 @@ import { useTranslation } from 'react-i18next';
 import {
   ActionFunctionArgs,
   LoaderFunctionArgs,
+  useNavigate,
   useParams,
 } from 'react-router-dom';
 import { useFilterVisiblePage } from '~/hooks';
 import { Page } from '~/models';
 import { useGetPagesFromWiki, wikiQueryOptions } from '~/services';
-import { useOpenDeleteModal, useOpenRevisionModal } from '~/store';
+import {
+  useOpenDeleteModal,
+  useOpenRevisionModal,
+  useUserRights,
+  useWikiActions,
+} from '~/store';
 
 const RevisionModal = lazy(
   async () => await import('~/features/page/RevisionModal/RevisionModal')
@@ -67,10 +82,13 @@ export const Pages = () => {
   const openVersionsModal = useOpenRevisionModal();
   const openDeleteModal = useOpenDeleteModal();
 
+  const navigate = useNavigate();
+  const userRights = useUserRights();
   const isDesktopDevice = useMediaQuery('only screen and (min-width: 1024px)');
 
   const [selectedPages, setSelectedPages] = useState<string[]>([]);
 
+  const { setOpenRevisionModal, setOpenDeleteModal } = useWikiActions();
   const { isPending, data, error } = useGetPagesFromWiki({
     wikiId: params.wikiId!,
     content: false,
@@ -82,6 +100,104 @@ export const Pages = () => {
   const filteredData = data
     ?.filter((page) => filterVisiblePage(page))
     .sort((a, b) => b.modified.$date - a.modified.$date);
+
+  const selectedPagesCount = selectedPages.length;
+
+  const isOnlyRead =
+    userRights.read &&
+    !userRights.contrib &&
+    !userRights.creator &&
+    !userRights.manager;
+
+  const items: ToolbarItem[] = [
+    {
+      type: 'icon',
+      name: 'read',
+      props: {
+        icon: <See />,
+        onClick: () =>
+          navigate(`/id/${params.wikiId}/page/${selectedPages[0]}`),
+        disabled: selectedPagesCount !== 1,
+        'aria-label': t('wiki.list.toolbar.action.read'),
+      },
+      tooltip: {
+        message: t('wiki.list.toolbar.action.read'),
+        position: 'bottom',
+      },
+    },
+    {
+      type: 'icon',
+      name: 'move',
+      props: {
+        icon: <FolderMove />,
+        disabled: selectedPagesCount < 1 || selectedPagesCount > 2,
+        'aria-label': t('wiki.list.toolbar.action.move'),
+      },
+      visibility: isOnlyRead ? 'hide' : 'show',
+      tooltip: {
+        message: t('wiki.list.toolbar.action.move'),
+        position: 'bottom',
+      },
+    },
+    {
+      type: 'icon',
+      name: 'duplicate',
+      props: {
+        icon: <Copy />,
+        disabled: selectedPagesCount !== 1,
+        'aria-label': t('wiki.list.toolbar.action.duplicate'),
+      },
+      visibility: isOnlyRead ? 'hide' : 'show',
+      tooltip: {
+        message: t('wiki.list.toolbar.action.duplicate'),
+        position: 'bottom',
+      },
+    },
+    {
+      type: 'icon',
+      name: 'history',
+      props: {
+        icon: <Forgoing />,
+        onClick: () => setOpenRevisionModal(true),
+        disabled: selectedPagesCount !== 1,
+        'aria-label': t('wiki.list.toolbar.action.history'),
+      },
+      visibility: isOnlyRead ? 'hide' : 'show',
+      tooltip: {
+        message: t('wiki.list.toolbar.action.history'),
+        position: 'bottom',
+      },
+    },
+    {
+      type: 'icon',
+      name: 'print',
+      props: {
+        icon: <Print />,
+        disabled: selectedPagesCount !== 1,
+        'aria-label': t('wiki.list.toolbar.action.print'),
+      },
+      visibility: isOnlyRead ? 'hide' : 'show',
+      tooltip: {
+        message: t('wiki.list.toolbar.action.print'),
+        position: 'bottom',
+      },
+    },
+    {
+      type: 'icon',
+      name: 'delete',
+      props: {
+        icon: <Delete />,
+        disabled: selectedPagesCount < 1,
+        onClick: () => setOpenDeleteModal(true),
+        'aria-label': t('wiki.list.toolbar.action.delete'),
+      },
+      visibility: isOnlyRead ? 'hide' : 'show',
+      tooltip: {
+        message: t('wiki.list.toolbar.action.delete '),
+        position: 'bottom',
+      },
+    },
+  ];
 
   const renderDesktopNode = (
     item: Page,
@@ -164,8 +280,10 @@ export const Pages = () => {
       </Heading>
       <div className="px-md-16">
         <List
+          items={items}
           data={filteredData}
           renderNode={isDesktopDevice ? renderDesktopNode : renderMobileNode}
+          onSelectedItems={setSelectedPages}
         />
         <Suspense fallback={<LoadingScreen position={false} />}>
           {openVersionsModal && <RevisionModal pageId={selectedPages[0]} />}
