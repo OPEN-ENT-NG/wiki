fa818708a0f76f66834ce03a873a3425359e5d55
david-cc <david.caudeli@web-education.net>
2024-10-01 14:41:07 +0200

    fix: #WB2-1925, redirect to a visible default page

---
+++ frontend/src/utils/findDefaultPage.ts
@@ -0,0 +1,20 @@
+import { RightRole } from 'edifice-ts-client';
+import { Page, Wiki } from '~/models';
+
+type UserRights = Record<RightRole, boolean>;
+
+export const findDefaultPage = (
+  wiki: Wiki,
+  userRights: UserRights
+): Page | undefined => {
+  const { pages, index } = wiki;
+  const indexPage = pages.find((page) => page._id === index);
+
+  // Return the index page if the user is a manager or if it's visible.
+  if (indexPage && (userRights.manager || indexPage.isVisible)) {
+    return indexPage;
+  }
+
+  // Return the first page if user is a manager or the first visible page
+  return userRights.manager ? pages[0] : pages.find((page) => page.isVisible);
+};
