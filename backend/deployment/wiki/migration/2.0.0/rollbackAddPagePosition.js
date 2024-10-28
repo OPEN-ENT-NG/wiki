d4dcf2ca29358bb544e6760220ea6172ebbc41ca
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2024-10-28 11:52:28 +0100

    chore: #WB2-2012, add rollback scripts

---
+++ backend/deployment/wiki/migration/2.0.0/rollbackAddPagePosition.js
@@ -0,0 +1,11 @@
+var countPageRollbacked = 0;
+
+db.wiki.find({}).forEach((wiki) => {
+  wiki.pages.forEach((page) => {
+    delete page.position;
+    countPageRollbacked++;
+  });
+  db.wiki.updateOne({ _id: wiki._id }, { $set: { pages: wiki.pages } });
+});
+
+print(`Nombre de pages rollback : ${countPageRollbacked}`);
