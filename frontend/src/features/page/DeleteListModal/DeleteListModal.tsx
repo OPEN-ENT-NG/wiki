402a5c6d4183c765054b1405b689d45776ca426f
Clement Creusat <clement.creusat@gmail.com>
2024-11-07 11:31:14 +0100

    fix: #WB2-2108, add new wording and check if selectedPages has children

--- frontend/src/features/page/DeleteListModal/DeleteListModal.tsx
+++ frontend/src/features/page/DeleteListModal/DeleteListModal.tsx
@@ -1,18 +1,32 @@
 import { Alert, Button, Modal } from '@edifice-ui/react';
 import { createPortal } from 'react-dom';
 import { useTranslation } from 'react-i18next';
-import { useFetcher } from 'react-router-dom';
+import { useFetcher, useParams } from 'react-router-dom';
+import { useGetWiki } from '~/services';
 import { useOpenDeleteModal, useUserRights, useWikiActions } from '~/store';
 
 const DeleteListModal = ({ selectedPages }: { selectedPages: string[] }) => {
   const openDeleteModal = useOpenDeleteModal();
   const userRights = useUserRights();
-
+  const params = useParams();
   const { t } = useTranslation('wiki');
   const { setOpenDeleteModal } = useWikiActions();
 
   const fetcher = useFetcher();
   const canManage = userRights.manager;
+
+  // Get the wiki data for the current wiki ID
+  const { data: wiki } = useGetWiki(params.wikiId!);
+
+  // Find any selected page that has children
+  const hasSelectedPagesWithChildren = wiki?.pages?.some(
+    (page) =>
+      selectedPages.includes(page._id) &&
+      page.children &&
+      page.children.length > 0,
+  );
+
+  const hasChildren = hasSelectedPagesWithChildren;
 
   return createPortal(
     <Modal
@@ -21,13 +35,24 @@ const DeleteListModal = ({ selectedPages }: { selectedPages: string[] }) => {
       onModalClose={() => setOpenDeleteModal(false)}
     >
       <Modal.Header onModalClose={() => setOpenDeleteModal(false)}>
-        {t('wiki.modal.delete.page.header')}
+        {hasChildren
+          ? t('wiki.modal.delete.pages.header')
+          : t('wiki.modal.delete.page.header')}
       </Modal.Header>
-      <Modal.Subtitle>{t('wiki.modal.delete.page.subtitle')}</Modal.Subtitle>
+      <Modal.Subtitle>
+        {hasChildren
+          ? t('wiki.modal.delete.pages.subtitle')
+          : t('wiki.modal.delete.page.subtitle')}
+      </Modal.Subtitle>
       <Modal.Body>
         {!canManage && (
           <Alert type="warning">
             {t('wiki.modal.delete.listpage.warning')}
+          </Alert>
+        )}
+        {hasChildren && (
+          <Alert type="warning">
+            {t('wiki.modal.delete.listpages.warning')}
           </Alert>
         )}
       </Modal.Body>
@@ -53,7 +78,9 @@ const DeleteListModal = ({ selectedPages }: { selectedPages: string[] }) => {
             value={JSON.stringify(selectedPages)}
             data-testid="delete-button"
           >
-            {t('wiki.modal.delete.page.btn')}
+            {hasChildren
+              ? t('wiki.modal.delete.pages.btn')
+              : t('wiki.modal.delete.page.btn')}
           </Button>
         </fetcher.Form>
       </Modal.Footer>
