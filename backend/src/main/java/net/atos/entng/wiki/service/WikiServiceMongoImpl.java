21bd727f1686e28072a522476222f18522a78f3d
david-cc <david.caudeli@web-education.net>
2024-11-26 11:42:42 +0100

    fix: fix WikiService java compile issue

--- backend/src/main/java/net/atos/entng/wiki/service/WikiServiceMongoImpl.java
+++ backend/src/main/java/net/atos/entng/wiki/service/WikiServiceMongoImpl.java
@@ -421,10 +421,10 @@ public class WikiServiceMongoImpl extends MongoDbCrudService implements WikiServ
 						.put("authorName", user.getUsername())
 						.put("modified", MongoDb.now())
 						.put("created", MongoDb.now())
-						.put("position", lastPosition);
+						// Set the position of the new page to the last position in the list
+						.put("position", lastPosition)
 						// a manager
 						.put("isVisible", newVisibility);
-						// Set the position of the new page to the last position in the list
 						
 						
 				// Tiptap Transformer
