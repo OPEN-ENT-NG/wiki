22f58defff8ca9930682ac1caba2553569af80dc
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2024-11-28 13:06:33 +0100

    feat: #WB2-2052, managers should see all pages

--- backend/src/main/java/net/atos/entng/wiki/service/WikiRepositoryEvents.java
+++ backend/src/main/java/net/atos/entng/wiki/service/WikiRepositoryEvents.java
@@ -20,54 +20,199 @@
 package net.atos.entng.wiki.service;
 
 import static net.atos.entng.wiki.Wiki.REVISIONS_COLLECTION;
-import static net.atos.entng.wiki.Wiki.WIKI_COLLECTION;
-
-import fr.wseduc.mongodb.MongoDb;
-import io.vertx.core.Vertx;
+import io.vertx.core.*;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonObject;
 import org.entcore.common.service.impl.MongoDbRepositoryEvents;
-import io.vertx.core.Handler;
 import io.vertx.core.json.JsonArray;
-import org.entcore.common.storage.Storage;
+import org.entcore.common.user.UserUtils;
+
+import java.util.*;
 
 public class WikiRepositoryEvents extends MongoDbRepositoryEvents {
+	private final EventBus eventBus;
+
+	public static final String MANAGER_RIGHT = "net-atos-entng-wiki-controllers-WikiController|shareWiki";
 
 	public WikiRepositoryEvents(Vertx vertx) {
-		super(vertx,"net-atos-entng-wiki-controllers-WikiController|shareWiki",
-				REVISIONS_COLLECTION, "wikiId");
+		super(vertx, MANAGER_RIGHT, REVISIONS_COLLECTION, "wikiId");
+		this.eventBus = vertx.eventBus();
 	}
+
+	/**
+	 * Get group ids for resources
+	 * @param resources
+	 * @return group ids
+	 */
+	private Set<String> getGroupIdsForResources(JsonArray resources) {
+		final Set<String> groupIds = new HashSet<>();
+		// Iterate over resources
+		for(final Object resource : resources){
+			if(resource instanceof JsonObject){
+				final JsonObject resourceObject = (JsonObject) resource;
+				// Get shared
+				final JsonArray shared = resourceObject.getJsonArray("shared", new JsonArray());
+				// Iterate over shared
+				for(final Object sharedObject : shared){
+					if(sharedObject instanceof JsonObject){
+						final JsonObject sharedJson = (JsonObject) sharedObject;
+						// Get group id
+						final String groupId = sharedJson.getString("groupId", "");
+						if(groupId != null && !groupId.isEmpty()){
+							// Add group id
+							groupIds.add(groupId);
+						}
+					}
+				}
+			}
+		}
+		return groupIds;
+	}
+
+	/**
+	 * Get user ids for group id
+	 * @param groupId
+	 * @param userId
+	 * @return user ids
+	 */
+	private Future<Set<String>> getUserIdsByGroupId(String groupId, final String userId) {
+		final Promise<Set<String>> promise = Promise.promise();
+		// Get user ids for group id
+		UserUtils.getUserIdsForGroupIds(Collections.singleton(groupId), userId, eventBus, userIds -> {
+			if(userIds.succeeded()){
+				promise.complete(userIds.result());
+			} else {
+				promise.fail(userId);
+			}
+		});
+		// Return user ids promise
+		return promise.future();
+	}
+
+	/**
+	 * Get user ids by group ids
+	 * @param groupIds
+	 * @param userId
+	 * @return user ids by group id
+	 */
+	private Future<Map<String, Set<String>>> getUserIdsByGroupIds(Set<String> groupIds, final String userId) {
+		// Get user ids for group ids
+		final Map<String, Future<Set<String>>> futures = new HashMap<>();
+		for(final String groupId : groupIds){
+			futures.put(groupId, getUserIdsByGroupId(groupId, userId));
+		}
+		return CompositeFuture.all(new ArrayList<>(futures.values())).map(v -> {
+			// Create map of user ids by group id
+			final Map<String, Set<String>> userIdsByGroupId = new HashMap<>();
+			// Iterate over user ids
+			for(final Map.Entry<String, Future<Set<String>>> entry : futures.entrySet()){
+				userIdsByGroupId.put(entry.getKey(), entry.getValue().result());
+			}
+			// Return user ids by group id
+			return userIdsByGroupId;
+		});
+	}
+
+	/**
+	 * Get manager group ids
+	 * @param shared
+	 * @return manager group ids
+	 */
+	private Set<String> getManagerGroupIds(JsonArray shared) {
+		final Set<String> groupIds = new HashSet<>();
+		// Iterate over shared
+		for(final Object sharedObject : shared){
+			if(sharedObject instanceof JsonObject){
+				final JsonObject sharedJson = (JsonObject) sharedObject;
+				// Get group id
+				final String groupId = sharedJson.getString("groupId", "");
+				// If group id is not null and not empty and has manager right, add group id
+				if(groupId != null && !groupId.isEmpty() && sharedJson.getBoolean(MANAGER_RIGHT, false)){
+					// Add group id
+					groupIds.add(groupId);
+				}
+			}
+		}
+		// Return group ids
+		return groupIds;
+	}
+
+	private Set<String> getManagerUserIds(JsonArray shared) {
+		// Get manager group ids
+		final Set<String> userIds = new HashSet<>();
+		// Iterate over shared
+		for(Object sharedObject : shared){
+			if(sharedObject instanceof JsonObject){
+				final JsonObject sharedJson = (JsonObject) sharedObject;
+				// Get user id
+				final String userId = sharedJson.getString("userId", "");
+				// If user id is not null and not empty and has manager right, add user id
+				if(userId != null && !userId.isEmpty() && sharedJson.getBoolean(MANAGER_RIGHT, false)){
+					// Add user id
+					userIds.add(userId);
+				}
+			}
+		}
+		return userIds;
+	}
+
 	/**
 	 * Filter out pages that are not visible
 	 * @param resources
 	 * @return filtered resources
 	 */
 	@Override
-	protected JsonArray exportResourcesFilter(JsonArray resources, String exportId, String userId) {
-		// Iterate over resources
-		for(final Object resource : resources){
-			if(resource instanceof JsonObject){
-				final JsonObject resourceObject = (JsonObject) resource;
-				// if owner of the wiki is the user, keep all pages
-				if(resourceObject.getJsonObject("owner", new JsonObject()).getString("userId", "").equals(userId)){
-					continue;
-				}
-				// Get pages
-				final JsonArray pages = resourceObject.getJsonArray("pages", new JsonArray());
-				// Filter out pages that are not visible or not authored by the user
-				final JsonArray filteredPages = new JsonArray();
-				for(final Object page : pages){
-					final JsonObject pageObject = (JsonObject) page;
-					final Boolean isVisible = pageObject.getBoolean("isVisible", false);
-					final String author = pageObject.getString("author", "");
-					// Keep page if it is visible or authored by the user
-					if(isVisible || author.equals(userId)){
-						filteredPages.add(page);
+	protected Future<JsonArray> exportResourcesFilter(JsonArray resources, String exportId, String userId) {
+		// Get group ids for resources
+		final Set<String> groupIds = getGroupIdsForResources(resources);
+		// Get user ids for group ids
+		return getUserIdsByGroupIds(groupIds, userId).map(userIdsByGroupId -> {
+			// Iterate over resources
+			for(final Object resource : resources){
+				if(resource instanceof JsonObject){
+					final JsonObject resourceObject = (JsonObject) resource;
+					// if owner of the wiki is the user, keep all pages
+					if(resourceObject.getJsonObject("owner", new JsonObject()).getString("userId", "").equals(userId)){
+						continue;
 					}
+					// Get shared
+					final JsonArray shared = resourceObject.getJsonArray("shared", new JsonArray());
+					// Get manager user ids
+					final Set<String> managerUserIds = getManagerUserIds(shared);
+					// Get manager group ids
+					final Set<String> managerGroupIds = getManagerGroupIds(shared);
+					// Iterate over manager group ids
+					for(final String managerGroupId : managerGroupIds){
+						if(userIdsByGroupId.containsKey(managerGroupId)){
+							// Add manager user ids
+							managerUserIds.addAll(userIdsByGroupId.get(managerGroupId));
+						}
+					}
+					// Get pages
+					final JsonArray pages = resourceObject.getJsonArray("pages", new JsonArray());
+					// Filter out pages that are not visible or not authored by the user
+					final JsonArray filteredPages = new JsonArray();
+					// Iterate over pages
+					for(final Object page : pages){
+						if(page instanceof JsonObject){
+							final JsonObject pageObject = (JsonObject) page;
+							// Get is visible
+							final Boolean isVisible = pageObject.getBoolean("isVisible", false);
+							// Get author
+							final String author = pageObject.getString("author", "");
+							// Keep page if it is visible or authored by the user or the user is a manager
+							if(isVisible || author.equals(userId) || managerUserIds.contains(userId)){
+								// Add page
+								filteredPages.add(page);
+							}
+						}
+					}
+					// Update pages
+					resourceObject.put("pages", filteredPages);
 				}
-				// Update pages
-				resourceObject.put("pages", filteredPages);
 			}
-		}
-		return resources;
+			// Return resources
+			return resources;
+		});
 	}
 }
