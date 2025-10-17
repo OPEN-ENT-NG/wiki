2ee72f305955baeec7570180422d1a2ca33236fd
david-cc <david.caudeli@web-education.net>
2025-10-17 17:22:32 +0200

    feat: open wiki when created and show pages assitant screen

---
+++ frontend/src/features/wiki/PagesAssistant/PagesAssistantTreeItem.tsx
@@ -0,0 +1,28 @@
+import { SortableTree } from '@edifice.io/react';
+
+export const PagesAssistantTreeItem = () => {
+  const treeData = [
+    {
+      id: '1',
+      name: 'Pages Assistant Overview',
+      title: 'Getting Started with Pages Assistant',
+    },
+  ];
+
+  return (
+    <SortableTree
+      nodes={treeData}
+      selectedNodeId="1"
+      renderNode={({ node }) => (
+        <div
+          className="d-flex flex-fill align-items-center justify-content-between"
+          style={{ width: '100%' }}
+        >
+          <span className="text-truncate">{node.name}</span>
+        </div>
+      )}
+      onSortable={() => {}}
+      onTreeItemClick={() => {}}
+    />
+  );
+};
