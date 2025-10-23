d0c76ac775a0c29502b89b80864a1ec347ee7e65
david-cc <david.caudeli@web-education.net>
2025-10-23 16:53:18 +0200

    feat: #PEDAGO-3338, add pages assistant dashboard page

---
+++ frontend/src/features/wiki/PagesAssistant/PagesAssistantRoot.tsx
@@ -0,0 +1,144 @@
+import { Button, Card, Grid, useEdificeClient } from '@edifice.io/react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router-dom';
+
+export const PagesAssistantRoot = () => {
+  const { appCode } = useEdificeClient();
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const handleNewPageButtonClick = () => {
+    navigate(`/id/${params.wikiId}/page/create`);
+  };
+
+  const handleAssitantAIButtonClick = () => {
+    navigate('ai');
+  };
+
+  return (
+    <div className="pages-assistant-wrapper mx-32">
+      <h2 className="my-32">{t('wiki.assistant.title', { ns: appCode })}</h2>
+      <Grid>
+        {/* MANUAL CREATION */}
+        <Grid.Col sm="6">
+          <Card isSelectable={false} isClickable={false} className="h-full">
+            <Card.Body>
+              <div className="text-truncate">
+                <Card.Title>
+                  {t('wiki.assistant.card.manual.title', { ns: appCode })}
+                </Card.Title>
+                <Card.Text className="white-space-normal">
+                  {t('wiki.assistant.card.manual.description', {
+                    ns: appCode,
+                  })}
+                </Card.Text>
+              </div>
+            </Card.Body>
+            <Card.Footer>
+              <Button
+                color="tertiary"
+                variant="ghost"
+                size="sm"
+                onClick={handleNewPageButtonClick}
+              >
+                {t('wiki.assistant.card.manual.button', {
+                  ns: appCode,
+                })}
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Grid.Col>
+
+        {/* AI ASSISTANT */}
+        <Grid.Col sm="6">
+          <Card isSelectable={false} isClickable={false}>
+            <Card.Body>
+              <div className="text-truncate">
+                <Card.Title>
+                  <p>{t('wiki.assistant.card.ai.title', { ns: appCode })}</p>
+                </Card.Title>
+                <Card.Text className="white-space-normal">
+                  {t('wiki.assistant.card.ai.description', {
+                    ns: appCode,
+                  })}
+                </Card.Text>
+              </div>
+            </Card.Body>
+            <Card.Footer>
+              <Button
+                color="tertiary"
+                variant="ghost"
+                size="sm"
+                onClick={handleAssitantAIButtonClick}
+              >
+                {t('wiki.assistant.card.ai.button', {
+                  ns: appCode,
+                })}
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Grid.Col>
+
+        {/* LIBRARY INSPIRATION */}
+        <Grid.Col sm="6">
+          <Card isSelectable={false} isClickable={false}>
+            <Card.Body>
+              <div className="text-truncate">
+                <Card.Title>
+                  <p>
+                    {t('wiki.assistant.card.library.title', { ns: appCode })}
+                  </p>
+                </Card.Title>
+                <Card.Text className="white-space-normal">
+                  {t('wiki.assistant.card.library.description', {
+                    ns: appCode,
+                  })}
+                </Card.Text>
+              </div>
+            </Card.Body>
+            <Card.Footer>
+              <Button color="tertiary" variant="ghost" size="sm">
+                {t('wiki.assistant.card.library.button', {
+                  ns: appCode,
+                })}
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Grid.Col>
+
+        {/* IMPORT */}
+        <Grid.Col sm="6">
+          <Card isSelectable={false} isClickable={false}>
+            <Card.Body>
+              <div className="text-truncate">
+                <Card.Title>
+                  <p>
+                    {t('wiki.assistant.card.import.title', { ns: appCode })}
+                  </p>
+                </Card.Title>
+                <Card.Text className="white-space-normal">
+                  {t('wiki.assistant.card.import.description', {
+                    ns: appCode,
+                  })}
+                </Card.Text>
+              </div>
+            </Card.Body>
+            <Card.Footer>
+              <Button color="tertiary" variant="ghost" size="sm">
+                {t('wiki.assistant.card.import.button.yes', {
+                  ns: appCode,
+                })}
+              </Button>
+              <Button color="tertiary" variant="ghost" size="sm">
+                {t('wiki.assistant.card.import.button.no', {
+                  ns: appCode,
+                })}
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Grid.Col>
+      </Grid>
+    </div>
+  );
+};
