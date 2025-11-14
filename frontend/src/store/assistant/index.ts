72bff14711694d54ec74c9bf6e036d88b9bb19ef
david-cc <david.caudeli@web-education.net>
2025-11-14 12:43:17 +0100

    feat: #PEDAGO-3348, call generate service from frontend app

--- frontend/src/store/assistant/index.ts
+++ frontend/src/store/assistant/index.ts
@@ -1,6 +1,5 @@
 import { createStore, useStore } from 'zustand';
 import {
-  PageContent,
   PagesAssistantAIFormValues,
   PageStructure,
 } from '~/services/api/assistant/assistant.types';
@@ -8,14 +7,12 @@ import {
 interface State {
   formValues: PagesAssistantAIFormValues;
   pagesStructure: PageStructure[];
-  pagesContent: PageContent[];
 }
 
 type Action = {
   actions: {
     setFormValues: (formData: PagesAssistantAIFormValues) => void;
     setPagesStructure: (pagesStructure: PageStructure[]) => void;
-    setPagesContent: (pagesContent: PageContent[]) => void;
   };
 };
 
@@ -24,7 +21,6 @@ type Params<U> = Parameters<typeof useStore<typeof store, U>>;
 const initialState: State = {
   formValues: { level: '', subject: '', sequence: '', keywords: '' },
   pagesStructure: [],
-  pagesContent: [],
 };
 
 const store = createStore<State & Action>()((set) => ({
@@ -34,7 +30,6 @@ const store = createStore<State & Action>()((set) => ({
       set({ formValues: formData }),
     setPagesStructure: (pagesStructure: PageStructure[]) =>
       set({ pagesStructure }),
-    setPagesContent: (pagesContent: PageContent[]) => set({ pagesContent }),
   },
 }));
 
@@ -48,7 +43,5 @@ export const useFormValuesStore = () =>
   useAssistantStore((state) => state.formValues);
 export const usePagesStructureStore = () =>
   useAssistantStore((state) => state.pagesStructure);
-export const usePagesContent = () =>
-  useAssistantStore((state) => state.pagesContent);
 export const usePagesAssistantActions = () =>
   useAssistantStore((state) => state.actions);
