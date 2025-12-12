9f48d41a8600730b1fba6c3c9d35e6c4edb007c8
david-cc <david.caudeli@web-education.net>
2025-12-12 15:43:56 +0100

    fix: #PEDAGO-3609, add Margins to AI Assistant pages

--- frontend/src/features/wiki/PagesAssistant/PagesAssistantAISteps/PagesAssistantAIStep1Form.tsx
+++ frontend/src/features/wiki/PagesAssistant/PagesAssistantAISteps/PagesAssistantAIStep1Form.tsx
@@ -79,132 +79,140 @@ export const PagesAssistantAIStep1Form = () => {
   };
 
   return (
-    <div className="pages-assistant-ai-wrapper mx-64 my-24">
-      <div>
-        <Stepper currentStep={0} nbSteps={3} />
-        <h2 className="mt-16">
-          {t('wiki.assistant.ai.step1.title', { ns: appCode })}
-        </h2>
-        <p className="text-gray-700">
-          {t('wiki.assistant.ai.step1.description', { ns: appCode })}
-        </p>
-      </div>
-
-      <div className="mt-24">
-        <Form
-          id="wikiAIForm"
-          key="wikiAIForm"
-          role="form"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <Flex direction="column" gap="24">
-            {/* LEVELS */}
-            <div>
-              <h3 className="mb-12">
-                {t('wiki.assistant.ai.step1.level', { ns: appCode })}
-              </h3>
-              {/* TODO: REFACTOR GRID & FLEX SYSTEM FOR LEVELS FOR BETTER RESPONSIVNESS */}
-              <Grid className="gap-8">
-                <Grid.Col sm="3" md="5" lg="5" xl="7">
-                  <span className="fw-bold pb-8">Collège</span>
-                </Grid.Col>
-                <Grid.Col sm="1" md="3" lg="3" xl="5">
-                  <span className="fw-bold pb-8">Lycée</span>
-                </Grid.Col>
-              </Grid>
-              <Flex gap="8">
-                {levelsData.map((level, index) => (
-                  <Controller
-                    key={index}
-                    name="level"
-                    control={control}
-                    rules={{ required: true }}
-                    render={({ field: { value, onChange } }) => (
-                      <SimpleRadioCard
-                        key={index}
-                        label={level.value}
-                        value={level.value}
-                        groupName="levels"
-                        selectedValue={value}
-                        onChange={(v) => {
-                          onChange(v);
-                          handleLevelChange();
-                        }}
-                      />
-                    )}
-                  />
-                ))}
-              </Flex>
-            </div>
-
-            {/* SUBJECTS */}
-            <div>
-              <h3 className="mb-12">
-                {t('wiki.assistant.ai.step1.subject', { ns: appCode })}
-              </h3>
-              <Grid className="gap-12">
-                {availableSubjects.sort().map((subject, index) => (
-                  <Grid.Col sm="2" md="4" lg="4" xl="3" key={index}>
-                    <Controller
-                      key={index}
-                      name="subject"
-                      control={control}
-                      rules={{ required: true }}
-                      render={({ field: { value, onChange } }) => (
-                        <SimpleRadioCard
-                          key={index}
-                          label={subject.value}
-                          value={subject.value}
-                          image={subject.image}
-                          groupName="subjects"
-                          selectedValue={value}
-                          onChange={(v) => {
-                            onChange(v);
-                          }}
-                        />
-                      )}
-                    />
-                  </Grid.Col>
-                ))}
-              </Grid>
-
-              {/* Subject suggestion "card" button */}
-              <Button
-                variant="outline"
-                color="tertiary"
-                leftIcon={<IconQuestion />}
-                onClick={handleSubjectSuggestionClick}
-                className="border-gray-400 mt-12"
-                style={{ height: '65px' }}
-              >
-                {t('wiki.assistant.ai.step1.subject.suggestion', {
-                  ns: appCode,
-                })}
-              </Button>
-            </div>
-          </Flex>
-
+    <Flex justify="center">
+      <div className="col-12 col-md-10 col-lg-8">
+        <div className="pages-assistant-ai-wrapper my-24">
           <div>
-            <Flex className="mt-24" justify="end">
-              <Button
-                variant="ghost"
-                color="tertiary"
-                leftIcon={<IconRafterLeft />}
-                onClick={handleBackButtonClick}
-              >
-                {t('previous')}
-              </Button>
-              <Button
-                type="submit"
-                rightIcon={<IconRafterRight />}
-                disabled={!isValid}
-              >
-                {t('next')}
-              </Button>
-            </Flex>
+            <Stepper currentStep={0} nbSteps={3} />
+            <h2 className="mt-16">
+              {t('wiki.assistant.ai.step1.title', { ns: appCode })}
+            </h2>
+            <p className="text-gray-700">
+              {t('wiki.assistant.ai.step1.description', { ns: appCode })}
+            </p>
           </div>
-        </Form>
+
+          <div className="mt-24">
+            <Form
+              id="wikiAIForm"
+              key="wikiAIForm"
+              role="form"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <Flex direction="column" gap="24">
+                {/* LEVELS */}
+                <div>
+                  <h3 className="mb-12">
+                    {t('wiki.assistant.ai.step1.level', { ns: appCode })}
+                  </h3>
+                  {/* TODO: REFACTOR GRID & FLEX SYSTEM FOR LEVELS FOR BETTER RESPONSIVNESS */}
+                  <Grid className="gap-8">
+                    <Grid.Col sm="3" md="5" lg="5" xl="7">
+                      <span className="fw-bold pb-8">Collège</span>
+                    </Grid.Col>
+                    <Grid.Col sm="1" md="3" lg="3" xl="5">
+                      <span className="fw-bold pb-8">Lycée</span>
+                    </Grid.Col>
+                  </Grid>
+                  <Flex gap="8">
+                    {levelsData.map((level, index) => (
+                      <Controller
+                        key={index}
+                        name="level"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field: { value, onChange } }) => (
+                          <SimpleRadioCard
+                            key={index}
+                            label={level.value}
+                            value={level.value}
+                            groupName="levels"
+                            selectedValue={value}
+                            onChange={(v) => {
+                              onChange(v);
+                              handleLevelChange();
+                            }}
+                          />
+                        )}
+                      />
+                    ))}
+                  </Flex>
+                </div>
+
+                {/* SUBJECTS */}
+                <div>
+                  <h3 className="mb-12">
+                    {t('wiki.assistant.ai.step1.subject', { ns: appCode })}
+                  </h3>
+                  <Grid className="gap-12">
+                    {availableSubjects.sort().map((subject, index) => (
+                      <Grid.Col sm="2" md="4" lg="4" xl="3" key={index}>
+                        <Controller
+                          key={index}
+                          name="subject"
+                          control={control}
+                          rules={{ required: true }}
+                          render={({ field: { value, onChange } }) => (
+                            <SimpleRadioCard
+                              key={index}
+                              label={subject.value}
+                              value={subject.value}
+                              image={subject.image}
+                              groupName="subjects"
+                              selectedValue={value}
+                              onChange={(v) => {
+                                onChange(v);
+                              }}
+                            />
+                          )}
+                        />
+                      </Grid.Col>
+                    ))}
+                  </Grid>
+
+                  {/* Subject suggestion "card" button */}
+                  <Grid className="gap-12">
+                    <Grid.Col sm="2" md="4" lg="4" xl="3">
+                      <Button
+                        variant="outline"
+                        color="tertiary"
+                        leftIcon={<IconQuestion />}
+                        onClick={handleSubjectSuggestionClick}
+                        className="d-flex justify-content-start border-gray-400 mt-12 w-100"
+                        style={{ height: '65px' }}
+                      >
+                        {t('wiki.assistant.ai.step1.subject.suggestion', {
+                          ns: appCode,
+                        })}
+                      </Button>
+                    </Grid.Col>
+                  </Grid>
+                </div>
+              </Flex>
+
+              <div>
+                <Flex className="mt-24" justify="end">
+                  <Button
+                    variant="ghost"
+                    color="tertiary"
+                    leftIcon={<IconRafterLeft />}
+                    onClick={handleBackButtonClick}
+                  >
+                    {t('previous')}
+                  </Button>
+                  <Button
+                    type="submit"
+                    rightIcon={<IconRafterRight />}
+                    disabled={!isValid}
+                  >
+                    {t('next')}
+                  </Button>
+                </Flex>
+              </div>
+            </Form>
+          </div>
+        </div>
       </div>
-    </div>
+    </Flex>
   );
 };
--- frontend/src/features/wiki/PagesAssistant/PagesAssistantAISteps/PagesAssistantAIStep2Form.tsx
+++ frontend/src/features/wiki/PagesAssistant/PagesAssistantAISteps/PagesAssistantAIStep2Form.tsx
@@ -72,110 +72,116 @@ export const PagesAssistantAIStep2Form = () => {
   };
 
   return (
-    <div className="pages-assistant-ai-wrapper mx-64 my-24">
-      <div>
-        <Stepper currentStep={1} nbSteps={3} />
-        <h2 className="mt-16">
-          {t('wiki.assistant.ai.step2.title', { ns: appCode })}
-        </h2>
-        <p className="text-gray-700">
-          {t('wiki.assistant.ai.step2.description', { ns: appCode })}
-        </p>
-      </div>
-      <div className="mt-24">
-        <Form
-          id="wikiAIForm"
-          key="wikiAIForm"
-          role="form"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          {/* SEQUENCE */}
+    <Flex justify="center">
+      <div className="col-12 col-md-10 col-lg-8">
+        <div className="pages-assistant-ai-wrapper my-24">
           <div>
-            <h3 className="mb-12">
-              {t('wiki.assistant.ai.step1.sequence', { ns: appCode })}
-            </h3>
-            <Controller
-              name="sequence"
-              control={control}
-              rules={{ required: true }}
-              render={({ field: { value, onChange } }) => (
-                <Select
-                  data-testid="wiki.assistant.ai.step1.select.sequence"
+            <Stepper currentStep={1} nbSteps={3} />
+            <h2 className="mt-16">
+              {t('wiki.assistant.ai.step2.title', { ns: appCode })}
+            </h2>
+            <p className="text-gray-700">
+              {t('wiki.assistant.ai.step2.description', { ns: appCode })}
+            </p>
+          </div>
+          <div className="mt-24">
+            <Form
+              id="wikiAIForm"
+              key="wikiAIForm"
+              role="form"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              {/* SEQUENCE */}
+              <div>
+                <h3 className="mb-12">
+                  {t('wiki.assistant.ai.step1.sequence', { ns: appCode })}
+                </h3>
+                <Controller
+                  name="sequence"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field: { value, onChange } }) => (
+                    <Select
+                      data-testid="wiki.assistant.ai.step1.select.sequence"
+                      size="md"
+                      selectedValue={value}
+                      onValueChange={(v) => {
+                        onChange(v);
+                      }}
+                      options={availableSequences}
+                      placeholderOption={t(
+                        'wiki.assistant.ai.step1.select.sequence',
+                        {
+                          ns: appCode,
+                        },
+                      )}
+                    />
+                  )}
+                />
+              </div>
+              <FormControl id="keywords" className="mb-24">
+                <Flex justify="between">
+                  <Label>
+                    {t('wiki.assistant.ai.step2.keywords.input.label', {
+                      ns: appCode,
+                    })}{' '}
+                    -{' '}
+                    <span className="text-gray-700 fst-italic ">
+                      {t('optional')}
+                    </span>
+                  </Label>
+                  <p className="small text-gray-700 p-2">
+                    {`${watch('keywords', '')?.length || 0} / 80`}
+                  </p>
+                </Flex>
+                <Input
+                  data-testid="communities-create-input-title"
+                  type="text"
+                  {...register('keywords', {
+                    maxLength: 80,
+                    pattern: {
+                      value: /[^ ]/,
+                      message: 'invalid title',
+                    },
+                  })}
                   size="md"
-                  selectedValue={value}
-                  onValueChange={(v) => {
-                    onChange(v);
-                  }}
-                  options={availableSequences}
-                  placeholderOption={t(
-                    'wiki.assistant.ai.step1.select.sequence',
+                  maxLength={80}
+                  placeholder={t(
+                    'wiki.assistant.ai.step2.keywords.input.placeholder',
                     {
                       ns: appCode,
                     },
                   )}
                 />
-              )}
-            />
+                <div className="mt-4 text-gray-700 fst-italic small">
+                  {t('wiki.assistant.ai.step2.keywords.legend', {
+                    ns: appCode,
+                  })}
+                </div>
+              </FormControl>
+              <div>
+                <Flex className="mt-auto" justify="end">
+                  <Button
+                    variant="ghost"
+                    color="tertiary"
+                    leftIcon={<IconRafterLeft />}
+                    onClick={handleBackButtonClick}
+                  >
+                    {t('previous')}
+                  </Button>
+                  <Button
+                    type="submit"
+                    rightIcon={<IconRafterRight />}
+                    disabled={!isValid}
+                  >
+                    {t('next')}
+                  </Button>
+                </Flex>
+              </div>
+            </Form>
           </div>
-          <FormControl id="keywords" className="mb-24">
-            <Flex justify="between">
-              <Label>
-                {t('wiki.assistant.ai.step2.keywords.input.label', {
-                  ns: appCode,
-                })}{' '}
-                -{' '}
-                <span className="text-gray-700 fst-italic ">
-                  {t('optional')}
-                </span>
-              </Label>
-              <p className="small text-gray-700 p-2">
-                {`${watch('keywords', '')?.length || 0} / 80`}
-              </p>
-            </Flex>
-            <Input
-              data-testid="communities-create-input-title"
-              type="text"
-              {...register('keywords', {
-                maxLength: 80,
-                pattern: {
-                  value: /[^ ]/,
-                  message: 'invalid title',
-                },
-              })}
-              size="md"
-              maxLength={80}
-              placeholder={t(
-                'wiki.assistant.ai.step2.keywords.input.placeholder',
-                {
-                  ns: appCode,
-                },
-              )}
-            />
-            <div className="mt-4 text-gray-700 fst-italic small">
-              {t('wiki.assistant.ai.step2.keywords.legend', { ns: appCode })}
-            </div>
-          </FormControl>
-          <div>
-            <Flex className="mt-auto" justify="end">
-              <Button
-                variant="ghost"
-                color="tertiary"
-                leftIcon={<IconRafterLeft />}
-                onClick={handleBackButtonClick}
-              >
-                {t('previous')}
-              </Button>
-              <Button
-                type="submit"
-                rightIcon={<IconRafterRight />}
-                disabled={!isValid}
-              >
-                {t('next')}
-              </Button>
-            </Flex>
-          </div>
-        </Form>
+        </div>
       </div>
-    </div>
+    </Flex>
   );
 };
