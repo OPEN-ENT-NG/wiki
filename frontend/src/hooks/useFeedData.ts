3a1ca55a97906899d20b07c51f7c8e842c7e9757
david-cc <david.caudeli@web-education.net>
2026-02-03 17:24:05 +0100

    fix: #PEDAGO-3616, fix loader icon for Manual pages

--- frontend/src/hooks/useFeedData.ts
+++ frontend/src/hooks/useFeedData.ts
@@ -1,10 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
-import { Page } from '~/models';
+import { Created, Page } from '~/models';
 import { useGetWiki } from '~/services';
 import { useTreeActions } from '~/store';
 import { getChildrenRecursively } from '~/utils/getChildrenRecursively';
 import { useFilterVisiblePage } from './useFilterVisiblePage';
+
+export interface FeedDataItem {
+  id: string;
+  name: string;
+  section: boolean;
+  position?: number;
+  isVisible: boolean;
+  aiMetadata?: {
+    contentGenerated: boolean;
+    contentGeneratedDate?: Created | undefined;
+  };
+  children?: {
+    id: string;
+    name: string;
+    isVisible: boolean;
+    position?: number;
+  }[];
+}
 
 export const useFeedData = () => {
   const params = useParams();
@@ -15,61 +33,64 @@ export const useFeedData = () => {
   const filterVisiblePage = useFilterVisiblePage();
   const filterParentPage = (page: Page): boolean => !page.parentId;
 
-  useEffect(() => {
-    if (data) {
-      const newTree = data.pages
-        .filter((page) => filterParentPage(page) && filterVisiblePage(page))
-        .map((page) => {
-          if (page.children) {
-            //TODO instead of flattening the tree, we should break parent children relationship at level 2 when moving a page
-            // Get all children of the page recursively (flatten the tree because we display only 2 levels)
-            const childPages = getChildrenRecursively({
-              page,
-              filterPage: filterVisiblePage,
-              allPages: data.pages,
-            });
-            // exclude the page itself from the children
-            delete childPages[page._id];
-
-            return {
-              id: page._id,
-              name: page.title,
-              section: true,
-              position: page.position,
-              isVisible: page.isVisible,
-              children: Object.values(childPages)
-                .slice()
-                .sort((a, b) => (a.position ?? 0) - (b.position ?? 0)),
-              aiMetadata: {
-                contentGenerated: page.aiMetadata?.contentGenerated || false,
-                contentGeneratedDate:
-                  page.aiMetadata?.contentGeneratedDate || undefined,
-              },
-            };
-          }
-          return {
-            id: page._id,
-            name: page.title,
-            section: true,
-            position: page.position,
-            isVisible: page.isVisible,
-            aiMetadata: {
-              contentGenerated: page.aiMetadata?.contentGenerated || false,
-              contentGeneratedDate:
-                page.aiMetadata?.contentGeneratedDate || undefined,
-            },
-          };
-        });
-
-      setTreeData(
-        newTree
-          .slice()
-          .sort(
-            (a, b) =>
-              (a.position ?? newTree.length) - (b.position ?? newTree.length),
-          ),
-      );
+  const newTree: FeedDataItem[] = useMemo(() => {
+    if (!data) {
+      return [];
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data]);
+
+    return data.pages
+      .filter((page) => filterParentPage(page) && filterVisiblePage(page))
+      .map((page) => {
+        const filteredPage: FeedDataItem = {
+          id: page._id,
+          name: page.title,
+          section: true,
+          position: page.position,
+          isVisible: page.isVisible,
+        };
+        const resultPage: FeedDataItem = filteredPage;
+
+        // Add aiMetadata if exists
+        if (page.aiMetadata) {
+          resultPage.aiMetadata = {
+            contentGenerated: page.aiMetadata?.contentGenerated || false,
+            contentGeneratedDate:
+              page.aiMetadata?.contentGeneratedDate || undefined,
+          };
+        }
+
+        // Add children if exists
+        if (page.children) {
+          //TODO instead of flattening the tree, we should break parent children relationship at level 2 when moving a page
+          // Get all children of the page recursively (flatten the tree because we display only 2 levels)
+          const childPages = getChildrenRecursively({
+            page,
+            filterPage: filterVisiblePage,
+            allPages: data.pages,
+          });
+          // exclude the page itself from the children
+          delete childPages[page._id];
+
+          resultPage.children = Object.values(childPages)
+            .slice()
+            .sort((a, b) => (a.position ?? 0) - (b.position ?? 0));
+        }
+        return resultPage;
+      });
+  }, [data, filterVisiblePage]);
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+
+    setTreeData(
+      newTree
+        .slice()
+        .sort(
+          (a, b) =>
+            (a.position ?? newTree.length) - (b.position ?? newTree.length),
+        ),
+    );
+  }, [data, newTree, setTreeData]);
 };
